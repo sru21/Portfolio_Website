71fb954a4876fd536b209cb376fdd45ff4d12f21
Sruti Kumari <srutikumari.0120@gmail.com>
2023-04-20 18:54:28 +0530

    First Commit

---
+++ Portfolio/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings:["Full Stack Developer", "Frontend Developer", "Java Programmer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
